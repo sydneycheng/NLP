a73c2206c8261707426fbe5c463c452e664bc230
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-10-20 17:07:17 -0500

    Romeo and Juliet commit

--- nlp1.py
+++ nlp1.py
@@ -1,9 +1,9 @@
 from textblob import TextBlob
 
-text = "Today is a beautiful day.  Tomorrow looks like bad weather."
-text = "I REALLY HATE YOU"
+# text = "Today is a beautiful day.  Tomorrow looks like bad weather."
+# text = "I REALLY HATE YOU"
 
-blob = TextBlob(text)
+# blob = TextBlob(text)
 
 # #functions
 # print(blob.sentences)
@@ -28,23 +28,71 @@ blob = TextBlob(text)
 #     print(sentence)
 #     print(round(sentence.sentiment.polarity, 3))
 
-from textblob.sentiments import NaiveBayesAnalyzer
+# from textblob.sentiments import NaiveBayesAnalyzer
 
-blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+# blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
 
-print(blob.sentiment)
+# print(blob.sentiment)
 
-for sentence in blob.sentences:
-    print(sentence.sentiment)
+# for sentence in blob.sentences:
+#     print(sentence.sentiment)
 
 
 
-spanish = blob.translate(to='es')
+# spanish = blob.translate(to='es')
 
-print(spanish)
+# print(spanish)
 
-chinese = blob.translate(to='zh')
+# chinese = blob.translate(to='zh')
 
-print(chinese)
+# print(chinese)
 
-print(chinese.translate())
+# print(chinese.translate())
+
+
+
+#changing words to their singular or plural form
+from textblob import Word
+
+index = Word('index')   #this creates a word object called index
+print(index.pluralize())
+
+cacti = Word('cacti')
+print(cacti.singularize())
+
+#wordlist
+animals = TextBlob('dog cat fish bird').words
+print(animals.pluralize())
+
+
+#spellcheck and correction
+word = Word('theyr')
+
+print(word.spellcheck())
+print(word.correct())
+
+#Normalization: lemmatization or stemming
+word1 = Word("studies")
+word2 = Word("varieties")
+
+print(word1.stem())
+print(word2.stem())
+
+print(word1.lemmatize())
+print(word2.lemmatize())
+
+#Definitions, Synonyms and Antonyms from WordNet
+happy = Word("happy")
+
+print(happy.definitions)
+print(happy.synsets)    # A set of synonyms
+
+for s in happy.synsets:
+    for l in s.lemmas():
+        print(l.name())
+
+synonym = happy.synsets[1].lemmas()[0].name()
+print(synonym)
+
+antonym = happy.synsets[0].lemmas()[0].lemmas()[0].antonyms()[0].name()
+print(antonym)
---
+++ nlp2.py
@@ -0,0 +1,63 @@
+from textblob import TextBlob
+import nltk
+from pathlib import Path
+import pandas as pd
+
+#nltk.download("stopwords")
+
+from nltk.corpus import stopwords
+
+stops = stopwords.words("english")
+#print(stops)
+
+blob = TextBlob("Today is a beautiful day.")
+#print(blob.words)
+
+#should only include today, beautiful, day
+#expression, iteration, condition
+cleanlist = [word for word in blob.words if word not in stops]
+
+#print(cleanlist)
+
+#Romeo and Juliet blob
+blob = TextBlob(Path("RomeoandJuliet.txt").read_text())
+
+print(blob.words.count("joy"))
+
+print(blob.word_counts["juliet"])
+
+#print(blob.noun_phrases.count("lady capulet"))
+
+print(blob.words.count("thou"))
+
+more_stops = ["thee", "thou", "thy"]
+
+stops += more_stops
+
+items = blob.word_counts.items()
+#print(items)
+
+clean_items = [i for i in items if i[0] not in stops]
+print(clean_items[:10])
+
+
+from operator import itemgetter
+sorted_list = sorted(clean_items, key=itemgetter(1), reverse=True)  #descending order
+
+print(sorted_list[:10])
+
+
+#grab top 20 words with the highest number
+top20 = sorted_list[:20]
+
+df = pd.DataFrame(top20, columns=["word","count"])
+print(df)
+
+
+import matplotlib.pyplot as plt
+##plot on a graph using dataframe
+df.plot.bar(x="word", y="count", legend=False, color=["y", "c", "m", "b", "g", "r"])
+
+plt.gcf().tight_layout()
+
+plt.show()
