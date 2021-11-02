44f0f1673dd9847fea267c1a1c0e87859c490384
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-11-01 22:06:43 -0500

    final johnHW commit

--- HW_NLP exercise.py
+++ HW_NLP exercise.py
@@ -5,10 +5,8 @@ from pathlib import Path
 import pandas as pd
 #nltk.download("stopwords")
 from nltk.corpus import stopwords
-
-#import matplotlib.pyplot as plt
-
 import imageio
+from operator import itemgetter
 from wordcloud import WordCloud
 
 ##create List of stopwords
@@ -17,42 +15,33 @@ more_stops = ["thy", "ye", "verily", "thee", "hath", "say", "thou", "art", "shal
 stops += more_stops
 
 
-
-
 ##import Book of John txt
 john = TextBlob(Path("book of John text.txt").read_text())
-#john = Path("book of John text.txt").read_text()
-#print(john.words)
-nouns = john.noun_phrases
-#cleanlist = [word for word in john.words if word not in stops if word in nouns]
-#print(cleanlist)
-
+nouns = []
+nouns += john.noun_phrases
 
 
 ##count word occurences and get top 15 most recurring words
 items = john.word_counts.items()
-#print(items)
-clean_items = [word for word in items if word[0] not in stops if word[0] in nouns]
-#print(clean_items[:15])
 
-from operator import itemgetter
+clean_items = [word for word in items if word[0] in nouns and word[0] not in stops]
 sorted_list = sorted(clean_items, key=itemgetter(1), reverse=True)
-print(sorted_list[:15])
 
-john15 = ""
-top15 = sorted_list[:15]
+top_sorted = str(sorted_list[:15])
+print(top_sorted)
 
-for x in top15[0]:
-    john15[0] += x
-    x + " "
-print(john15)
+top15 = " "
+for i in top_sorted:
+    top15 += i[0]
+    top15 += ""
+print(top15)
 
-##create mask_image using imageio
-#mask_image = imageio.imread('mask_heart.png')
 
-wordcloud = WordCloud(colormap='prism', background_color='white')
-wordcloud = wordcloud.generate(john15)
+#wordcloud
+wordcloud = WordCloud(colormap='prism', background_color='gray')
+wordcloud = wordcloud.generate(top15)
 wordcloud = wordcloud.to_file('BookOfJohnImage.png')
 
 print('done')
+
 
